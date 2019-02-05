160deea191d921a0539f34f5bc9d99d39b4ce39e
Belikov <Sergeybelik86@gmail.com>
2019-02-05 17:02:59 +0300

    2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,38 @@
 package com.company;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+    public static void main(String[] argv) {
 
-    public static void main(String[] args) {
-	// write your code here
+        PrintWriter pw;
+        Scanner sc;
+
+        sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        pw = new PrintWriter(System.out);
+        String[] b = a.split(" ");
+
+        for (int i = 1; i < b.length; i++){
+            while (b[0].length() > b[i].length()) {
+                StringBuffer c = new StringBuffer(b[i]);
+                c.insert(0, 0);
+                b[i] = c.toString();
+            }
+            while (b[i].length() > b[0].length()) {
+                StringBuffer c = new StringBuffer(b[0]);
+                c.insert(0, 0);
+                b[0] = c.toString();
+            }
+
+
+            if (b[0].compareTo(b[i]) < 0){
+                b[0] = b[i];
+            }
+        }
+        System.out.println(b[0]);
+
+        pw.close();
+
     }
 }
