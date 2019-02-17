a02e93b5437ba61f15c46121a01974786af0cfdc
Belikov <Sergeybelik86@gmail.com>
2019-02-17 21:02:18 +0300

    ДЗ №5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,31 +9,30 @@ import java.util.TreeSet;
 public class Main {
 
     public static void main(String[] args) {
-
+        // task №1
         long a = exponentiate(0, 0);
         System.out.println(a);
 
-
+        // task №2
         TreeSet<Thing> things = new TreeSet<Thing>((o1, o2) -> o2.getUnitCost().compareTo(o1.getUnitCost()));
+        setThings(things);
+        Bag bag = new Bag(50);
+        bag.fillBag(things);
+        Iterator<Thing> iterator = bag.getThings().iterator();
+        System.out.println("There are things in the bag:");
+        while (iterator.hasNext()){
+            System.out.println(iterator.next().getWeight());
+        }
+    }
 
-
+    private static void setThings(TreeSet things) {
 
         things.add(new Thing(10, 7));
         things.add(new Thing(8, 12));
         things.add(new Thing(14, 45));
-
-        Bag bag = new Bag(25);
-        bag.fillBag(things);
-
-//
-        Iterator<Thing> iterator = bag.getThings().iterator();
-
-        while (iterator.hasNext()){
-            System.out.println(iterator.next().getWeight());
-        }
-
-
+        things.add(new Thing(100, 800));
     }
+
     private static long exponentiate(int base, int exponent){
         if (exponent == 0) return 1;
         if (exponent < 2){
