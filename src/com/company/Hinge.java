fb4e99c4b3fd5b89c2d2380fcb5b833f8ee01c5a
Belikov <Sergeybelik86@gmail.com>
2019-02-27 21:09:07 +0300

    ДЗ №6 шарнир с коментариями

--- src/com/company/Hinge.java
+++ src/com/company/Hinge.java
@@ -29,8 +29,25 @@ public class Hinge {
         return hingeList;
     }
 
+    private void checkPath(int checkedVertex) {
+        cutVertex(checkedVertex);
+        dfp = new DepthFirstPaths(g, (int)adjArr[0]);
+        for (int i = 1; i < adjArr.length; i++) {
+            // поскольку все вершины из списка adjList проверяемой вершины гарантировано имеют как миниму один путь
+            // между собой (через проверяемую вершину) они находятся в одном компоненте связанности,
+            // если проверяемая вершина будет вырезана вегда будет верно:
+            // для любой вершины из adjList путь до одной или неслкольких других вершин из adjList
+            // не будет существовать, если эти вершины окажутся в разных компонентах связанности, т.е проверяемый узел
+            // будет являться шарниром
+            if (!dfp.hasPathTo((int)adjArr[i])) {
+                hingeList.add(checkedVertex);
+                return;
+            }
+        }
+    }
 
-
+    // обрезаем все ребра узла который хотим проверить на соответсвие условию шарнира
+    // и сохраняем убранные ребра для дальнейшего восстановления после проверки
     private void cutVertex(int vertex) {
         temp = new LinkedList<>();
         for (int i = 0; i < adjArr.length; i++){
@@ -39,17 +56,7 @@ public class Hinge {
         }
     }
 
-    private void checkPath(int checkedVertex) {
-        cutVertex(checkedVertex);
-        dfp = new DepthFirstPaths(g, (int)adjArr[0]);
-        for (int i = 1; i < adjArr.length; i++) {
-            if (!dfp.hasPathTo((int)adjArr[i])) {
-                hingeList.add(checkedVertex);
-                return;
-            }
-        }
-    }
-
+    // восстановление ребер после проверки
     private void restoreEdge(int vertex) {
 
         for (int i = 0; i < adjArr.length; i++){
